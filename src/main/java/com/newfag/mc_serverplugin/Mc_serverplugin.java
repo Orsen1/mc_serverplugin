692e32b2b88178bc40ded89e2db1be55f4b9fa8e
Arsenii Heorhinov <chip200511@gmail.com>
2025-11-10 22:24:50 +0100

    git init

---
+++ src/main/java/com/newfag/mc_serverplugin/Mc_serverplugin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.newfag.mc_serverplugin;
+
+/**
+ *
+ * @author 0rsen
+ */
+public class Mc_serverplugin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
